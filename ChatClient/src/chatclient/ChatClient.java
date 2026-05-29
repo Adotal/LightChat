6e3d3f3bb6d7f4ccc3c5bc62df8af5fdf24b7ca0
Adotal <adotal1484@gmail.com>
2026-05-29 17:51:56 -0600

    Added Hello World sample apps in two projects
    	Projects folders are
    		-ChatClient
    		-ChatServer
    	Also added .gitignore ignoring not wanted files

---
+++ ChatClient/src/chatclient/ChatClient.java
@@ -0,0 +1,17 @@
+package chatclient;
+
+/**
+ * @author adotal
+ * 
+ */
+public class ChatClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+    }
+    
+}
---
+++ ChatServer/src/chatserver/ChatServer.java
@@ -0,0 +1,17 @@
+package chatserver;
+
+/**
+ * @author adotal
+ * 
+ */
+public class ChatServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                
+        System.out.println("Hello World");
+    }
+    
+}
